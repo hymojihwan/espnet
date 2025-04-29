be9c5ced0bc52e5bed17439a0e8a37998a7d0a1f
momojihwan <momojihwan@gmail.com>
2025-04-29 16:48:41 +0900

    libri 360h

---
+++ egs2/librispeech_360/asr1/run_ot.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set="train_clean_360"
+valid_set="dev"
+test_sets="test_clean test_other"
+
+asr_task="asr_ot_transducer"
+asr_config=conf/tuning/transducer/OT/conformer-rnnt-ot-streaming.yaml
+inference_config=conf/tuning/transducer/decode_transducer.yaml
+inference_asr_model=valid.loss.ave_10best.pth
+
+./asr.sh \
+    --lang en \
+    --ngpu 4 \
+    --nj 16 \
+    --gpu_inference true \
+    --inference_nj 16 \
+    --nbpe 2048 \
+    --max_wav_duration 30 \
+    --asr_task "${asr_task}" \
+    --speed_perturb_factors "0.9 1.0 1.1" \
+    --feats_type raw \
+    --use_lm false \
+    --asr_config "${asr_config}" \
+    --inference_config "${inference_config}" \
+    --inference_asr_model "${inference_asr_model}" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --lm_train_text "data/${train_set}/text" \
+    --bpe_train_text "data/${train_set}/text" "$@"
